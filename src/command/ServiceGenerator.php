b20b921c902df5cb635794b6b586180ed7d5c715
houlinlin <houlinlin@kaituocn.com>
2020-09-03 11:00:21 +0800

    修复Model 没有表时继承的bug

--- src/command/ServiceGenerator.php
+++ src/command/ServiceGenerator.php
@@ -41,7 +41,7 @@ class ServiceGenerator
         $model_class = $controller . 'Model';
         $namespace->addUse('app\index\model\\' . $model_class);
         $namespace->addUse('\think\facade\Request');
-        $namespace->addUse('\Linphp\ServiceController\notice\\Msg');
+        $namespace->addUse('\Linphp\Generator\notice\\Msg');
         $class = $namespace->addClass($tableName_public_name);
         $class->addExtend(BaseController::class);
         #class内部注解
