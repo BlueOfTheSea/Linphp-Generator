5361cf81cb6b5fdc381eb44c85b2719ef77cd661
houlinlin <houlinlin@kaituocn.com>
2020-09-17 13:56:18 +0800

    修复app/model实体类同步

--- src/command/StartGenerator.php
+++ src/command/StartGenerator.php
@@ -44,6 +44,7 @@ class StartGenerator extends Command
     public function start($name)
     {
         if (!$name) {
+            $this->deldir(app_path() . 'model\\entity');
             (new EntityModelGenerator())->command();
             return;
         }
@@ -81,6 +82,36 @@ class StartGenerator extends Command
                 #生成控制器
                 $ControllerGenerator->command($class_name[0], $tableNameVal, $tableName);
             }
+        }
+    }
+
+    /**
+     * 删除model/entity实体模型
+     * @param $dir
+     * @return bool
+     * @author Administrator
+     */
+    public function deldir($dir)
+    {
+        //先删除目录下的文件：
+        $dh = opendir($dir);
+        while ($file = readdir($dh)) {
+            if ($file != "." && $file != "..") {
+                $fullpath = $dir . "/" . $file;
+                if (!is_dir($fullpath)) {
+                    unlink($fullpath);
+                } else {
+                    deldir($fullpath);
+                }
+            }
+        }
+
+        closedir($dh);
+        //删除当前文件夹：
+        if (rmdir($dir)) {
+            return true;
+        } else {
+            return false;
         }
     }
 
